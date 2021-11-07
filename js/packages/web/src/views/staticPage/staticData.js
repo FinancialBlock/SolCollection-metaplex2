2ad8ef67401c3d65a8fe839846b2d656979105fb
FinancialBlock <40401193+FinancialBlock@users.noreply.github.com>
2021-11-07 20:09:13 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/packages/web/src/views/staticPage/staticData.js
@@ -0,0 +1,22 @@
+export const data = {
+  headContent: {
+    title: 'We are SolCollection',
+    subtitle: 'About Us',
+    bannerImage: '/NFT.png',
+  },
+  leftContent: undefined,
+  midContent: {
+    sections: [
+      {
+        title: 'SolCollection is a protocol built on top of Solana that allows:',
+        paragraphs: [
+          '• Creating/Minting non-fungible tokens;',
+          '• Starting a variety of auctions for primary/secondary sales;',
+          '• and Visualizing NFTs in a standard way across wallets and applications.',
+        ],
+        image: '/main-banner.png',
+      },
+    ],
+  },
+  bottomContent: true,
+};
